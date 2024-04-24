2fbc00229e93bc0517965cee469b99cd97f7cad3
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-04-24 12:01:39 +0200

    Added home-button to navbar

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,12 +1,14 @@
 import React, {} from "react"
 import styles from "./navbar.module.css"
-import {Home,} from "react-feather"
+import { Home } from "react-feather";
 
 export default function Navbar() {
   return(
     <nav className={styles.navbarContainer}>
       <div className={styles.navbarContent}>
+        <button className={styles.iconButton}>
         <Home />
+        </button>
       </div>
     </nav>
   );
