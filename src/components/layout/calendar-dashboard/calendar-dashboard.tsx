a4b06b9e5ae6afa5f6a4ff1788e176d9c2c6762a
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-05-27 15:47:09 +0200

    Styling of Calendar

--- src/components/layout/calendar-dashboard/calendar-dashboard.tsx
+++ src/components/layout/calendar-dashboard/calendar-dashboard.tsx
@@ -4,6 +4,7 @@ import Calendar from "@/components/layout/calendar/calendar";
 export default function CalendarDashboard() {
   return (
     <div className={styles.calendarDashboard}>
+      <div className={styles.header}></div>
       <Calendar />
     </div>
   );
