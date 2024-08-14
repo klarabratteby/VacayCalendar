9c4ceeaf1b72ea54da49c49a8ccd3d2d1ae3421b
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-08-14 10:04:43 +0200

    Added Tabs for MY CALENDAR and FRIENDS CALENDAR

--- src/components/layout/calendar-dashboard/calendar-dashboard.tsx
+++ src/components/layout/calendar-dashboard/calendar-dashboard.tsx
@@ -1,11 +1,35 @@
+"use client";
+import React, { useState } from "react";
 import styles from "./calendar-dashboard.module.css";
 import Calendar from "@/components/layout/calendar/calendar";
 
 export default function CalendarDashboard() {
+  const [activeTab, setActiveTab] = useState("myCalendar"); // set default view to "My Calendar"
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
   return (
     <div className={styles.calendarDashboard}>
-      <div className={styles.header}></div>
-      <Calendar />
+      <div className={styles.header}>
+        <div className={styles.tabContainer}>
+          <button
+            className={`${styles.tab} ${activeTab === "myCalendar" ? styles.activeTab : ""}`}
+            onClick={() => handleTabClick("myCalendar")}
+          >
+            MY CALENDAR
+          </button>
+          <button
+            className={`${styles.tab} ${activeTab === "friendsCalendar" ? styles.activeTab : ""}`}
+            onClick={() => handleTabClick("friendsCalendar")}
+          >
+            FRIENDS CALENDAR
+          </button>
+        </div>
+      </div>
+
+      {activeTab === "myCalendar" && <Calendar />}
+      {activeTab === "friendsCalendar" && <Calendar />}
     </div>
   );
 }
