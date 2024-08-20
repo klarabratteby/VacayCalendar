fde3cb2a7edeb25db65579f4357b9de03b4a7ed7
klarabratteby <93648999+klarabratteby@users.noreply.github.com>
2024-08-20 15:54:34 +0200

    Resolved typo in navbar

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,39 +1,35 @@
-"use client"
-import React, {} from "react"
-import styles from "./navbar.module.css"
-import { Home, User, LogOut} from "react-feather";
-import Link from "next/link"
-import {usePathname} from "next/navigation";
-import {SignOut} from "@/components/auth/signOut";
-
+"use client";
+import React from "react";
+import styles from "./navbar.module.css";
+import { Home, User, LogOut } from "react-feather";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { SignOut } from "@/components/auth/signout";
 
 export default function Navbar() {
   const pathname = usePathname();
-  const {handleSignOut} = SignOut();
-  
+  const { handleSignOut } = SignOut();
 
-  return(
+  return (
     <nav className={styles.navbarContainer}>
       <div className={styles.navbarContent}>
         <Link href="/">
-        <button className={styles.iconButton}>
-          <Home />
-        </button>
+          <button className={styles.iconButton}>
+            <Home />
+          </button>
         </Link>
         {pathname === "/calendar" ? (
           <button className={styles.iconButton} onClick={handleSignOut}>
             <LogOut />
           </button>
         ) : (
-        <Link href="/login">
-        <button className={styles.iconButton}>
-          <User />
-        </button>
-        </Link>
+          <Link href="/login">
+            <button className={styles.iconButton}>
+              <User />
+            </button>
+          </Link>
         )}
       </div>
     </nav>
   );
 }
-
-
